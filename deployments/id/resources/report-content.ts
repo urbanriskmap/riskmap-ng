ae26c658a994c733f391339affe8cd35693ec8e4
ojha-url <mayank.ojha@gmail.com>
2018-03-23 18:04:45 -0400

    Added side-pane > report-content structure

---
+++ deployments/id/resources/report-content.ts
@@ -0,0 +1,30 @@
+export default {
+  items: [
+    {
+      name: 'Project overview',
+      icons: [
+        'project-logo' // Fontello name icon-project-logo
+      ],
+      videoLink: 'http_link_to_youtube_video',
+      steps: [
+        'step 1',
+        'step 2',
+        'step n'
+      ]
+    },
+    {
+      name: 'Social media apps',
+      icons: [
+        'facebook',
+        'twitter',
+        'local-apps'
+      ],
+      videoLink: 'http_link_to_youtube_video',
+      steps: [
+        'step 1',
+        'step 2',
+        'step n'
+      ]
+    }
+  ]
+};
--- src/app/map/side-pane/report-content/report-content.component.ts
+++ src/app/map/side-pane/report-content/report-content.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import reportContent from '../../../../resources/report-content';
+
 @Component({
   selector: 'app-report-content',
   templateUrl: './report-content.component.html',
   styleUrls: ['./report-content.component.scss']
 })
 export class ReportContentComponent implements OnInit {
+  content: {
+    name: string,
+    icons: string[],
+    videoLink: string,
+    steps: string[]
+  }[];
 
-  constructor() { }
+  constructor() {
+    this.content = reportContent.items;
+  }
 
   ngOnInit() {
   }
