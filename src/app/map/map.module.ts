5aab7a065e43798b5666d1f54b2368d875f16156
ojha-url <mayank.ojha@gmail.com>
2018-04-11 10:52:17 -0400

    Strict type checking

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -3,25 +3,29 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 
+// Components
+import { MapComponent } from './map.component';
+import { AreaInfoComponent } from './area-info/area-info.component';
+import { InfraInfoComponent } from './infra-info/infra-info.component';
 import { ReportInfoComponent } from './report-info/report-info.component';
 import { SensorInfoComponent } from './sensor-info/sensor-info.component';
-import { AreaInfoComponent } from './area-info/area-info.component';
 import { ScreenPopupComponent } from './screen-popup/screen-popup.component';
 import { SidePaneComponent } from './side-pane/side-pane.component';
-import { MapComponent } from './map.component';
-
-import { CustomMaterialsModule } from '../custom-materials.module';
-
-import { TimeService } from '../services/time.service';
-import { LayerService } from '../services/layer.service';
-import { HttpService } from '../services/http.service';
-import { SensorService } from '../services/sensor.service';
-import { InteractionService } from '../services/interaction.service';
-import { InfraInfoComponent } from './infra-info/infra-info.component';
 import { ReportContentComponent } from './side-pane/report-content/report-content.component';
 import { MapContentComponent } from './side-pane/map-content/map-content.component';
 import { InfoContentComponent } from './side-pane/info-content/info-content.component';
+
+// Modules
+import { CustomMaterialsModule } from '../custom-materials.module';
+
+// Services
 import { ChartService } from '../services/chart.service';
+import { HttpService } from '../services/http.service';
+import { InteractionService } from '../services/interaction.service';
+import { LayerService } from '../services/layer.service';
+import { SensorService } from '../services/sensor.service';
+import { TimeService } from '../services/time.service';
+
 
 @NgModule({
   imports: [
