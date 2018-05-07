21ace4131b15ef31241e0429ec639d20a34bd667
ojha-url <mayank.ojha@gmail.com>
2018-05-07 15:12:18 -0400

    Added reorient pipe to rotate images using exif data

--- src/app/map/image-preview/image-preview.component.ts
+++ src/app/map/image-preview/image-preview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { EXIF } from 'exif-js';
 
 import { SanitizePipe } from '../../pipes/sanitize.pipe';
+import { ReorientPipe } from '../../pipes/reorient.pipe';
 
 @Component({
   selector: 'app-image-preview',
@@ -11,26 +12,8 @@ import { SanitizePipe } from '../../pipes/sanitize.pipe';
 export class ImagePreviewComponent implements OnInit {
   @Input() imgUrl: string;
 
-  // transformations = {
-  //   1: '',
-  //   2: 'rotateY(180deg)',
-  //   3: 'rotate(180deg)',
-  //   4: 'rotate(180deg) rotateY(180deg)',
-  //   5: 'rotate(270deg) rotateY(180deg)',
-  //   6: 'rotate(90deg)',
-  //   7: 'rotate(90deg) rotateY(180deg)',
-  //   8: 'rotate(270deg)'
-  // };
-
   constructor() { }
 
   ngOnInit() {
-  }
-
-  getExif(): any {
-    const img = document.getElementById('fullSizeImage');
-    EXIF.getData(img, () => {
-      console.log(EXIF.getAllTags(this));
-    });
   }
 }
---
+++ src/app/pipes/reorient.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ReorientPipe } from './reorient.pipe';
+
+describe('ReorientPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ReorientPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
