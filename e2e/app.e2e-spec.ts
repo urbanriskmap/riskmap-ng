b564154cab68af89a5fd4103bfc574b7d942041d
abrahamq <abrahamquintero2323@gmail.com>
2018-06-28 13:06:29 -0400

    title check working

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('riskmap-ng App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display title', () => {
+    page.navigateHome();
+    expect(page.getTitle()).toEqual('RiskMap.in');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,23 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
+  navigateHome() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getTitle() {
+    return browser.getTitle();
+  }
+
+  navigateToJakarta() {
+    return browser.get('/jakarta');
+  }
+
+  clickLanguageMenu() {
+    return element(by.buttonText('Bahasa')).click();
+  }
+
+  clickBahasa() {
+    return element(by.css('.mat-option-text')).click();
   }
 }
