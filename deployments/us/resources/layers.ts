d151741b778b9493791b41c70dfc1793e6387eb7
asbarve <adityasbarve@gmail.com>
2018-10-19 11:29:05 -0400

    noaa sensor public access false

--- deployments/us/resources/layers.ts
+++ deployments/us/resources/layers.ts
@@ -198,7 +198,7 @@ export default {
         name: 'sensors_noaa',
         server: 'sensors',
         path: '',
-        publicAccess: true,
+        publicAccess: false,
         flags: [{agency: 'noaa'}],
         responseType: 'geojson',
         uniqueKey: 'id',
